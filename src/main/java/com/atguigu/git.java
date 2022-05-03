cfc7e971a872774b2891ea693b0803ae7b6d0928
Nibbles <2297191475@qq.com>
2022-05-03 18:03:26 +0800

    hot fix commit1

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -6,12 +6,12 @@ package com.atguigu;
  */
 public class git {
 
-
-
-
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+
+        System.out.println("hell0 git4");
+        System.out.println("hell0 git5");
     }
 }
