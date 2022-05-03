0faf7f2c603b4e6fee3a2b5c44690e27ab7e6105
Nibbles <2297191475@qq.com>
2022-05-03 17:37:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @project: gitTest
+ * @author: LYH
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
